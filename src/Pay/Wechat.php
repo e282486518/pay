852584935feac8234566d70ccccde5696abdfe19
longfei <phphome@qq.com>
2026-04-27 10:48:04 +0800

    兼容获取ip地址方案

--- src/Pay/Wechat.php
+++ src/Pay/Wechat.php
@@ -983,17 +983,6 @@ class Wechat
      */
     public static function get_ip()
     {
-        if (getenv("HTTP_CLIENT_IP"))
-            $ip = getenv("HTTP_CLIENT_IP");
-        else if(getenv("HTTP_X_FORWARDED_FOR"))
-            $ip = getenv("HTTP_X_FORWARDED_FOR");
-        else if(getenv("REMOTE_ADDR"))
-            $ip = getenv("REMOTE_ADDR");
-        else $ip = "Unknow";
-
-        if(preg_match('/^((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1 -9]?\d))))$/', $ip))
-            return $ip;
-        else
-            return '';
+        return SafeRequest::getip();
     }
 }
--- src/SafeRequest.php
+++ src/SafeRequest.php
@@ -244,4 +244,25 @@ class SafeRequest
             die();
         }
 	}
+
+    public static function getip()
+	{
+        // 改为
+        if (self::is_laravel()) {
+            return request()->ip();
+        } else {
+	        if (getenv("HTTP_CLIENT_IP"))
+	            $ip = getenv("HTTP_CLIENT_IP");
+	        else if(getenv("HTTP_X_FORWARDED_FOR"))
+	            $ip = getenv("HTTP_X_FORWARDED_FOR");
+	        else if(getenv("REMOTE_ADDR"))
+	            $ip = getenv("REMOTE_ADDR");
+	        else $ip = "Unknow";
+
+	        if(preg_match('/^((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1 -9]?\d))))$/', $ip))
+	            return $ip;
+	        else
+	            return '';
+        }
+	}
 }
